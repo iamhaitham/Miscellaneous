001dacee125525940d68d125809e2c43c882fefb
iamhaitham <iamhaitham2011@gmail.com>
2022-06-26 20:31:17 +0300

    Add more styles to ProductCategory component

--- Thinking in React/src/ProductCategory/ProductCategory.js
+++ Thinking in React/src/ProductCategory/ProductCategory.js
@@ -4,9 +4,10 @@ function ProductCategory(props) {
     const { categoryName } = props;
 
     return (
-      <>
+      <div className='productCategoryAsGrid'>
+        <span></span>
         <p className='boldCategory'>{categoryName}</p>
-      </>
+      </div>
     );
   }
   
