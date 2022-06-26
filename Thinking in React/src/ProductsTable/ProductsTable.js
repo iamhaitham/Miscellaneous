183cb8c25b02e721dc64e5a3a32dff22bdf0e1e9
iamhaitham <iamhaitham2011@gmail.com>
2022-06-26 20:10:35 +0300

    Create ProductsTable component

---
+++ Thinking in React/src/ProductsTable/ProductsTable.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Products from '../Products/Products';
+import ProductCategory from '../ProductCategory/ProductCategory';
+import ProductsTableHeaders from "../ProductsTableHeaders/ProductsTableHeaders";
+
+function ProductsTable() {
+  const products = [
+    {'id': 1, 'name': 'Apple', 'price': '$1', 'isInStock': true, 'category': 'Fruits'},
+    {'id': 2, 'name': 'Dragonfruit', 'price': '$1', 'isInStock': true, 'category': 'Fruits'},
+    {'id': 3, 'name': 'Passionfruit', 'price': '$2', 'isInStock': false, 'category': 'Fruits'},
+    {'id': 4, 'name': 'Spinach', 'price': '$2', 'isInStock': true, 'category': 'Vegetables'},
+    {'id': 5, 'name': 'Pumpkin', 'price': '$4', 'isInStock': false, 'category': 'Vegetables'},
+    {'id': 6, 'name': 'Peas', 'price': '$1', 'isInStock': true, 'category': 'Vegetables'}
+  ];
+
+  const uniqueCategories = Array.from(new Set(products.map(p => p.category)));
+
+  const categories = uniqueCategories.map((uc, idx) => {
+    return {'id': idx, 'name': uc}
+  });
+
+  let allProducts = [];
+
+  for(let i = 0; i < categories.length; i++) {
+    allProducts.push(
+      <React.Fragment key={categories[i].id}>
+        <ProductCategory categoryName={categories[i].name}/>
+        <Products categoryName={categories[i].name} products={products}/>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <>
+        <ProductsTableHeaders/>
+        {allProducts}
+    </>
+  );
+}
+
+export default ProductsTable;
