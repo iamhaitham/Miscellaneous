500fc6e45f2d10d49bafa0da783fb7d357ce23d9
iamhaitham <iamhaitham2011@gmail.com>
2022-06-26 20:10:57 +0300

    Create ProductCategory component

---
+++ Thinking in React/src/ProductCategory/ProductCategory.js
@@ -0,0 +1,15 @@
+
+
+function ProductCategory(props) {
+    const { categoryName } = props;
+
+    return (
+      <>
+        <p>{categoryName}</p>
+      </>
+    );
+  }
+  
+  export default ProductCategory;
+  
+  
