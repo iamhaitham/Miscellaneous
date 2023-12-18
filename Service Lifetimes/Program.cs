0fa3fa71f8767ca26c6cd5b5f8b6cddac3f3d068
iamhaitham <iamhaitham2011@gmail.com>
2023-12-18 18:39:30 +0200

    Create a new solution "Service Lifetimes"

---
+++ Service Lifetimes/Program.cs
@@ -0,0 +1,28 @@
+using Service_Lifetimes.Services.Implementations;
+using Service_Lifetimes.Services.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
